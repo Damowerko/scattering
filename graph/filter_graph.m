ff2c61b115b9e31246593e79e50b2c141153a6f9
Damian Owerko <damowerko@gmail.com>
2017-07-17 16:17:10 -0400

    Gridsearch on mnist

--- graph/filter_graph.m
+++ graph/filter_graph.m
@@ -1,29 +1,53 @@
 function [filters] = filter_graph(A, options)
 
 G = graph(A);
-L = laplacian(G);
 
-[V, E] = eig(full(L));
-%[E, I] = sort(diag(E), 'ascend');
-%V = V(:,I);
-%E = diag(E);
-emax = max(real(diag(E)));
-[E,I] = sort(abs(emax-diag(E)),'ascend');
-E = diag(E);
-V = V(:,I);
+switch options.graph_shift
+    case 'adjacency'
+        S = adjacency(G);
+    case 'laplacian'
+        S = laplacian(G);
+    case 'covariance'
+        load('cov');
+        S = C;
+    otherwise
+        error('Invalid graph_shift option.')
+end
+
+% get eigenvectors + eigenvalues and sort them lowest frequency first
+[V, E] = eig(full(S));
+if strcmpi(options.graph_shift, 'laplacian') | strcmpi(options.graph_shift, 'covariance')
+    [E, I] = sort(diag(E), 'ascend');
+    frequencies = E;
+    E = diag(E);
+elseif strcmpi(options.graph_shift, 'adjacency')
+    % sort complex eigenvalues
+    emax = max(real(diag(E)));
+    [frequencies,I] = sort(abs(emax-diag(E)),'ascend');
+    E = diag(E);
+    E = diag(E(I));
+end
+V = V(:,I); % sort eigenvectors
 
 % filters
 filters = struct();
 filters.psi = {options.J};
 filters.phi = [];
 
-disp('Generating psi');
 for j = 0:options.J-1
-    hf = morlet_1d_freq(length(A), options.psi.sigma, j)';
+    if options.lambda_scale
+        hf = morlet_1d_freq(length(A), options.psi.sigma, j)';
+    else
+        hf = morlet_1d_graph(frequencies, options.psi.sigma, j)';
+    end
     filters.psi{j+1} = freq_filter(hf, V, E);
 end
-disp('Generating phi');
-hf = gaussian_filter_freq(length(A), options.phi.sigma)';
+
+if options.lambda_scale
+    hf = gaussian_filter_freq(length(A), options.phi.sigma)';
+else
+    hf = gaussian_filter_graph(frequencies, options.psi.sigma)
+end
 filters.phi = freq_filter(hf, V, E);
 
 end
--- graph/mnist.m
+++ graph/mnist.m
@@ -16,29 +16,45 @@ test_samples = 1000;
 x_test = x_test(:,:,1:test_samples);
 y_test = y_test(1:test_samples);
 
+darr = [1 10:10:200];
+if exist('results/temp.mat', 'file')
+    load 'results/temp.mat'
+else
+    result_acc = zeros(length(darr),7,3);
+    result_loss = zeros(length(darr),7,3);
+end
+ 
+for J = 1:7
+for M = 1:3
+
+if J < M | (result_acc(length(darr),J,M) ~= 0 & result_loss(length(darr), J, M) ~= 0)
+    continue;
+end
+
+disp(sprintf('j=%d  |  M=%d', J, M))
 
 clear options;
-options.J = 4;
-options.M = 2;
+options.J = J;
+options.M = M;
 options.psi.sigma = 0.85;
 options.psi.xi = 3/4*pi;
 options.phi.sigma = 0.85;
+options.graph_shift = 'covariance'; % 'laplacian' or 'adjacency' or 'covariance'
+options.lambda_scale = true;
 
 P = 0;
 for m = 0:options.M
     P = P + nchoosek(options.J, m);
 end
 P = P * 28 * 28;
-
-disp 'generating filters'
-
 filters = filter_graph(image_adj(28,28), options);
 
-disp 'scattering transform'
-
+disp 'Transforming training samples...'
+progress = textprogressbar(train_samples);
 train_transform = zeros(P, size(x_train,3));
 for i = 1:size(x_train,3)
-    disp(['Test sample ' int2str(i) '/' int2str(size(x_train,3))])
+    progress(i);
+    
     x = x_train(:,:,i);
     x = x(:);
     
@@ -52,9 +68,12 @@ for i = 1:size(x_train,3)
     train_transform(:,i) = y(:);
 end
 
+disp 'Transforming testing samples...'
+progress = textprogressbar(test_samples);
 test_transform = zeros(P, size(x_test,3));
 for i = 1:size(x_test,3)
-    disp(['Test sample ' int2str(i) '/' int2str(size(x_test,3))])
+    progress(i);
+    
     x = x_test(:,:,i);
     x = x(:);
     
@@ -72,44 +91,63 @@ end
 v = cell(10, 1);
 m = cell(10, 1);
 
+disp 'Training classifier...'
+progress = textprogressbar(10);
+
 for n = 1:10
-    disp(['training: ' int2str(n) '/10'])
     s = train_transform(:, y_train == (n-1));
     m{n} =  mean(s,2);
     [V,E] = eig(cov((bsxfun(@minus,s,m{n}))'));
     [E,I] = sort(diag(E),'descend');
     v{n} = V(:,I);
+    progress(n);
 end
 
-disp 'evaluating'
-%% Evaluations
-d = 40; % number of PCA coefficients to use
-E = zeros(10,size(x_test,2));
-error = zeros(test_samples, 10);
-for n = 1:10
-    s = bsxfun(@minus, test_transform, m{n});
-    pc = v{n}(:,1:d);
-    err_temp = zeros(size(pc,2)+1, size(s,2));
-	err_temp(1,:) = sum(abs(s).^2,1);
-	err_temp(2:end,:) = -abs(pc'*s).^2;
-    error(:, n) = transpose(sum(err_temp, 1));
-    %err = sqrt(cumsum(err,1));
+disp 'Evaluation'
+progress = textprogressbar(length(darr));
+for dn = 1:length(darr)
+    %% Evaluations
+    d = darr(dn); % number of PCA coefficients to use
+    E = zeros(10,size(x_test,2));
+    error = zeros(test_samples, 10);
+    for n = 1:10
+        s = bsxfun(@minus, test_transform, m{n});
+        pc = v{n}(:,1:d);
+        err_temp = zeros(size(pc,2)+1, size(s,2));
+	    err_temp(1,:) = sum(abs(s).^2,1);
+	    err_temp(2:end,:) = -abs(pc'*s).^2;
+        error(:, n) = transpose(sum(err_temp, 1));
+        %err = sqrt(cumsum(err,1));
+    end
+
+
+    % find probability
+    prob = bsxfun(@rdivide, error, sum(error, 2));
+    prob = 1 - prob;
+
+    % logical index actual classes
+    Y_test = repmat(y_test(:),1,10) == repmat(1:10,length(y_test),1);
+    Y_test = transpose(Y_test);
+
+    logloss = -sum(log(prob(Y_test))) / size(y_test,1);
+    %disp(['Logloss: ' num2str(logloss)])
+
+    % accuracy
+    [~,I] = sort(error,2, 'ascend');
+    correct = I(:,1) == y_test+1;
+    accuracy = sum(correct) / length(correct);
+    %disp(['Accuracy: ' num2str(accuracy)]);
+
+    result_loss(dn, J, M) = logloss; % save results
+    result_acc(dn, J, M) = accuracy;
+    
+    save(['results/temp'],'result_loss','result_acc');
+    progress(dn); % update progress bar
+end
+end
 end
 
-disp 'done!'
-% find probability
-prob = bsxfun(@rdivide, error, sum(error, 2));
-prob = 1 - prob;
-
-% logical index actual classes
-Y_test = repmat(y_test(:),1,10) == repmat(1:10,length(y_test),1);
-Y_test = transpose(Y_test);
-
-logloss = -sum(log(prob(Y_test))) / size(y_test,1);
-disp(['Logloss: ' num2str(logloss)])
-
-% accuracy
-[~,I] = sort(error,2, 'ascend');
-correct = I(:,1) == y_test+1;
-accuracy = sum(correct) / length(correct);
-disp(['Accuracy: ' num2str(accuracy)]);
+if exist('results/temp.mat', 'file')
+    delete 'results/temp.mat'
+end
+save(['results/' datestr(datetime)],'result_loss','result_acc');
